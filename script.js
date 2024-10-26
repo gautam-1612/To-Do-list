fba58ea25b5025383e1d164ee0c14d4ee6a82560
gautam-1612 <gautambhushan5@gmail.com>
2024-10-26 21:54:44 +0530

    Update List Functionality.
    Update already present list items directly now from interface.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let add = document.getElementsByClassName("add")[0];
 let count = 0;
-
 add.addEventListener("click", () => {
     if (count < 4) {
         let newItem = prompt("Add Your new list item !");
@@ -20,4 +19,12 @@ Delete.addEventListener("click", () => {
     let elem = document.querySelector("ol");
     let number = parseInt(prompt("Enter the number of the list you want to Delete !"));
     elem.removeChild(elem.children[number - 1]);
+})
+
+let Update = document.getElementsByClassName("update")[0];
+Update.addEventListener("click", ()=>{
+    let number = parseInt(prompt("Enter the List number you want to modify or edit."));
+    let element = document.querySelector("ol");
+    let string = prompt("Enter the updated list item.");
+    element.children[number-1].innerText = string;
 })
